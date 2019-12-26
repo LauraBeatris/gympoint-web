35e540f9411781b753dcdaf2a6ef48f723ade13e
LauraBeatris <laurabeatriserafim@gmail.com>
2019-12-26 09:20:05 -0300

    global styles

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import './config/ReactotronConfig';
 import history from './services/history';
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
